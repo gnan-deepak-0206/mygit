5e8d18d0517c60745acb9d97cf7578d79b4ffc5f
Deepak <gnandeepak.g25@iiits.in>
2025-12-10 19:22:48 +0530

    name by loop

--- practice.c
+++ practice.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello world! \n My name is G.Gnan Deepak");
-    return 0;
+    for(int i=0;i<5;i++)
+    {
+        printf("Hello world! \n My name is G.Gnan Deepak");
+    }
+        return 0;
 }
